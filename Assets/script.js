f1b8f1f74b2c692470ace5f7d0db0030cac3ec12
Blauwe-Zebra <100291174+Blauwe-Zebra@users.noreply.github.com>
2025-03-13 23:45:39 +0100

    Form finisht with js to swith to login and sign up form

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+function Login() {
+  document.getElementById("Login").style.display = "none";
+  document.getElementById("SignUp").style.display = "flex";
+}
+
+function SignUp() {
+  document.getElementById("SignUp").style.display = "none";
+  document.getElementById("Login").style.display = "flex";
+}
