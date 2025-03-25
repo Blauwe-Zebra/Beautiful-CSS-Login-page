0e9fec484f6d4e2ac66e358d5e51e60311a28efb
Blauwe-Zebra <100291174+Blauwe-Zebra@users.noreply.github.com>
2025-03-25 20:20:19 +0100

    show and hide password

--- Assets/script.js
+++ Assets/script.js
@@ -7,3 +7,15 @@ function SignUp() {
   document.getElementById("SignUp").style.display = "none";
   document.getElementById("Login").style.display = "flex";
 }
+
+function Showpwd() {
+  document.getElementById("ShowPwdBtn").classList.toggle("fa-eye-slash");
+
+  if (
+    document.getElementById("ShowPwdBtn").classList.toggle("fa-eye") == true
+  ) {
+    document.getElementById("Password").type = "text";
+  } else {
+    document.getElementById("Password").type = "password";
+  }
+}
